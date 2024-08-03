27a71fbc61fee1816f128c7cb0d332c1770908cc
waheed ur Rahman <waheedkhan957759@gmail.com>
2024-08-03 17:16:28 +0500

    Create 13.listMethod.py

---
+++ 13.listMethod.py
@@ -0,0 +1,14 @@
+w = [1, 2, 44, 55, 211,99]
+
+# w.append(101)
+# w.sort(reverse=True)
+# w.reverse()
+# print(w.index(55))
+# print(w.count(55))
+# w.insert(1, 33)
+
+'''l = w.copy()
+l[0] = 55555
+print(l)'''
+
+print(w)
