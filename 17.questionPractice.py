e3364af57e6275d063bf64293e13fcd1b434144d
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-08-09 16:05:57 +0500

    hands on python

--- 17.questionPractice.py
+++ 17.questionPractice.py
@@ -1,19 +1,19 @@
-# questions = [
-#     {"question": "What is the capital city of Australia?", "answers": ["a: Islamabad", "b: Canberra", "c: Cairo", "d: Sydney"], "correct": "b"},
-#     {"question": "Who wrote the play \"Romeo and Juliet\"?", "answers": ["a: William Shakespeare", "b: Charles Dickens", "c: George Orwell", "d: Virginia Woolf"], "correct": "a"},
-#     {"question": "What is the largest planet in our solar system?", "answers": ["a: Mercury", "b: Venus", "c: Mars", "d: Jupiter"], "correct": "d"},
-#     {"question": "Which element has the chemical symbol 'O'?", "answers": ["a: Osmium", "b: Oxygen", "c: Oganesson", "d: Osmium"], "correct": "b"},
-#     {"question": "In which year did the Titanic sink?", "answers": ["a: 1990", "b: 1991", "c: 1992", "d: 1989"], "correct": "c"}
-# ]
+questions = [
+    {"question": "What is the capital city of Australia?", "answers": ["a: Islamabad", "b: Canberra", "c: Cairo", "d: Sydney"], "correct": "b"},
+    {"question": "Who wrote the play \"Romeo and Juliet\"?", "answers": ["a: William Shakespeare", "b: Charles Dickens", "c: George Orwell", "d: Virginia Woolf"], "correct": "a"},
+    {"question": "What is the largest planet in our solar system?", "answers": ["a: Mercury", "b: Venus", "c: Mars", "d: Jupiter"], "correct": "d"},
+    {"question": "Which element has the chemical symbol 'O'?", "answers": ["a: Osmium", "b: Oxygen", "c: Oganesson", "d: Osmium"], "correct": "b"},
+    {"question": "In which year did the Titanic sink?", "answers": ["a: 1990", "b: 1991", "c: 1992", "d: 1989"], "correct": "c"}
+]
 
-# for q in questions:
-#     print(q["question"])
-#     for ans in q["answers"]:
-#         print(ans)
-#     user_answer = input("Enter the letter for your answer: ").strip().lower()
-#     if user_answer == q["correct"]:
-#         print("Correct answer")
-#     else:
-#         print("Incorrect answer")
-#     print()  # Print a blank line for readability
+for q in questions:
+    print(q["question"])
+    for ans in q["answers"]:
+        print(ans)
+    user_answer = input("Enter the letter for your answer: ").strip().lower()
+    if user_answer == q["correct"]:
+        print("Correct answer")
+    else:
+        print("Incorrect answer")
+    print()  # Print a blank line for readability
 
