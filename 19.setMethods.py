d936dff299403afa94871351458f28cc0d81fc89
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-08-13 15:30:18 +0500

    Create 19.setMethods.py

---
+++ 19.setMethods.py
@@ -0,0 +1,38 @@
+#    Simple Set
+# s = {0,1,2,3,4,"waheed"}
+# print(s)
+
+
+#   METHODS
+
+     # UNOIN     
+# countries1 = {"Pakistan", "India", "China"}
+# countries2 = {"Japan", "Dubai", "Malta"}
+# countries3 = countries1.update(countries2)
+# print(countries1)
+# print(countries2)
+# print(countries3)
+ 
+     # UPDATE
+# countries1 = {"Pakistan", "India", "China"}
+# countries2 = {"Japan", "Dubai", "Malta"}
+# countries1.update(countries2)
+# print(countries1)
+# print(countries2)
+# print(countries1)
+
+     # intersection 
+# num1 = {1, 2, 3, 4, 5}
+# num2 = {6, 7, 2, 9, 5}
+# num3 = num1.intersection(num2)
+# print("Num 1 : ",num1 )
+# print("Num 2 : ",num2 )
+# print("Num 3 : ",num3 )
+
+     # intersection_update
+# num1 ={0, 1, 2}     
+# num2 ={3, 0, 4}     
+# num1.intersection_update(num2)
+# print(num1)
+# print(num2)
+
