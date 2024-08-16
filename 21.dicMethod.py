4d75bca05b201d5681a77a2a28c902ce1f07c823
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-08-16 17:27:04 +0500

    Add files via upload

---
+++ 21.dicMethod.py
@@ -0,0 +1,9 @@
+lib = {"book1": "Atomic Habits", "book2": "kite Runner", "book3": "47 law of power"}
+# print(lib)
+# lib.pop("book1")
+# lib.popitem()
+# lib.clear()   
+# print(lib.values())
+# lib.update({"book":"Escape the Matrix"})
+# print(lib.keys())
+print(lib)
