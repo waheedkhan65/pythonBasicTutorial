a4e3bf7a8accadb2daf7f6588c1344b9071c0592
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-08-17 15:25:12 +0500

    Add files via upload

---
+++ 23.exceptionHandling.py
@@ -0,0 +1,18 @@
+'''
+Exception handling in Python is a mechanism to manage errors or "exceptions" that occur during the execution of a 
+program. Rather than allowing the program to crash, you can "handle" these exceptions gracefully, providing an 
+alternative flow or informative error messages to the user.
+'''
+
+num = input("Enter any number for table : ")
+print(f"Multiplication table of {num} is : ")
+try:
+   for i in range (1, 11):
+    print(f"{num} x {i} = {int(num)*i}")
+except:
+  print("Invalid input")
+ 
+ 
+
+
+ 
