403c6dbe9d957acbdf8b0a9ef4289928bb95754f
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-08-19 17:27:08 +0500

    Add files via upload

---
+++ 26.inLineElseIf.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter first number "))
+num2 = int(input("Enter secound number "))
+print(f"The number {num1} is greater") if num1>num2 else print(f"The number {num2} is greater")
+print(f"The number one is : {num1} and secound is : {num2}")
