f3ff2ecc0b9da8917c6548c922dc68bfbadfff01
waheed Ur Rahman <144592652+waheedkhan65@users.noreply.github.com>
2024-09-04 15:47:09 +0500

    Add files via upload

---
+++ 28.handsOnOSModule.py
@@ -0,0 +1,29 @@
+import os
+
+# if not os.path.exists("practice222"):
+#  os.mkdir("practice222")
+
+# for i in range (0, 10):
+#     os.mkdir(f"practice222/day {i+1}")
+
+
+#  For rename a file 
+
+# for i in range(0,10):
+#    os.rename(f"practice222/day {i+1}", f"practice222/tutorial {i+1}")
+
+
+#  For list a 
+# files = os.listdir("practice222")
+# print(files)
+
+# for file in files:
+#     print(file)
+#     print(os.listdir(f"practice222/{file}"))
+
+# nm = "harry"
+# print(nm[-4:-2])
+
+nm = "waheed khan"
+print(len(nm))
+print(nm[-4:16])
